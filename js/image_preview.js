3ffa4b39164e2776101f60f65004d3ab212deea5
chernyshof <dmitriy.chernyshof@gmail.com>
2017-11-29 20:05:14 +0200

    Add arrows for carousel

--- js/image_preview.js
+++ js/image_preview.js
@@ -1,9 +1,9 @@
 let isPreviewing = false;
 let imagesUrls = [
-  'punoko.png',
+  'koncha.png',
   'tpfufun.jpg',
   'vohiyo.png',
-  'koncha.png',
+  'punoko.png',
   'tehepelo.jpeg',
 ];
 
@@ -51,7 +51,6 @@ function carousel(e) {
     let previewUrls = imagesUrls.slice()
     let img = preview.src.split('/').pop();
     let i = previewUrls.indexOf(img);
-    console.log(i);
     if (e.keyCode == 37) {
       i -= 1;
       if (i == -1) {
@@ -69,3 +68,45 @@ function carousel(e) {
 }
 
 document.addEventListener('keydown', carousel, true);
+
+function handleClickArrows(e) {
+  let className = e.currentTarget.querySelector('i').className;
+  if (className.indexOf('right') !== -1) {
+    imagesUrls.push(imagesUrls.shift());
+    renderImages();
+  } else if (className.indexOf('left') !== -1) {
+    imagesUrls.unshift(imagesUrls.pop());
+    renderImages();
+  }
+}
+
+function handleClickPreviewArrows(e) {
+  let className = e.currentTarget.querySelector('i').className;
+  let preview = document.getElementById('imagePreview');
+  let previewUrls = imagesUrls.slice()
+  let img = preview.src.split('/').pop();
+  let i = previewUrls.indexOf(img);
+  if (className.indexOf('right') !== -1) {
+    i += 1;
+    if (i > images.length) {
+      i = 0;
+    }
+    preview.src = `img/${imagesUrls[i]}`;
+  } else if (className.indexOf('left') !== -1) {
+    i -= 1;
+    if (i == -1) {
+      i = imagesUrls.length-1;
+    }
+    preview.src = `img/${imagesUrls[i]}`;
+  }
+}
+
+let arrows = document.getElementsByClassName('arrowblock');
+for (let i=0; i < arrows.length; i++) {
+  arrows[i].onclick = handleClickArrows;
+}
+
+let preview_arrows = document.getElementsByClassName('preview_arrowblock');
+for (let i=0; i < arrows.length; i++) {
+  preview_arrows[i].onclick = handleClickPreviewArrows;
+}
