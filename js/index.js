9eaa2e8d147d5b2ee210bef83bf9112abe7d9fc8
chernyshof <dmitriy.chernyshof@gmail.com>
2017-11-30 11:18:25 +0200

    add fade animation

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ function dynamicallyLoadScript(url) {
     document.head.appendChild(script);
 }
 
+dynamicallyLoadScript('js/fade.js')
 dynamicallyLoadScript('js/hint.js')
 dynamicallyLoadScript('js/shortcuts.js')
 dynamicallyLoadScript('js/spoilers.js')
 dynamicallyLoadScript('js/image_preview.js')
 dynamicallyLoadScript('js/tables.js')
+
